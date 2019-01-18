3ff55e0b98ce1e2f41ffaad06b4adae16ab3cab7
nanctsak <chantardsak@gmail.com>
2019-01-18 18:39:48 +0700

    add FrontEnd -close #2

---
+++ frontend/src/app/hospital-service/hospital-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hospital-service,',
+  templateUrl: './hospital-service.component.html',
+  styleUrls: ['./hospital-service.component.css']
+})
+export class HospitalServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
